9d19ea4703bb68c4aad27150340df853c76b6b17
ashaInfilogix <ashainfilogix@gmail.com>
2024-04-09 17:00:53 +0530

    edit and delete news and events

--- app/Http/Controllers/NewsAndEventsController.php
+++ app/Http/Controllers/NewsAndEventsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\NewsAndEvents;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use File;
 
 class NewsAndEventsController extends Controller
 {
@@ -62,24 +63,53 @@ class NewsAndEventsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(NewsAndEvents $newsAndEvents)
+    public function edit(NewsAndEvents $newsAndEvents, $id)
     {
-        //
+        $newsAndEvent = NewsAndEvents::findOrFail($id);
+
+        return view('news-and-events.edit', compact('newsAndEvent'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, NewsAndEvents $newsAndEvents)
+    public function update(Request $request, NewsAndEvents $newsAndEvents, $id)
     {
-        //
+        $oldImage = NULL;
+        $newsAndEvents = NewsAndEvents::findOrFail($id)->first();
+        if($newsAndEvents != '') {
+            $oldImage = $newsAndEvents->image;
+        }
+
+        if ($request->hasFile('image'))
+        {
+           $file = $request->file('image');
+           $filename = time().'.'.$file->getClientOriginalExtension();
+           $file->move(public_path('uploads/news-and-events/'), $filename);
+
+           $image_path = public_path($oldImage);
+            if(File::exists($image_path)) {
+                File::delete($image_path);
+            }
+        }
+
+        $newsAndEvents = NewsAndEvents::findOrFail($id)->update([
+            'title' => $request->title,
+            'image' =>  isset($filename) ? 'uploads/news-and-events/'. $filename : $oldImage,
+            'url'   => $request->url,
+            'text'  => $request->text
+        ]);
+
+        return Redirect::route("news-and-events.index")->with('success-message','News And Events Updated Successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(NewsAndEvents $newsAndEvents)
+    public function destroy(NewsAndEvents $newsAndEvents, $id)
     {
-        //
+        $newsAndEvents = NewsAndEvents::findOrFail($id)->delete();
+
+        return Redirect::route("news-and-events.index")->with('success-message','News And Events Deleted Successfully.');
     }
 }
---
+++ resources/views/news-and-events/edit.blade.php
@@ -0,0 +1,82 @@
+<x-app-layout>
+    <div class="py-12">
+
+        <div class="py-6">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="flex justify-between">
+                    <h1 class="font-semibold text-2xl text-gray-800 leading-tight">
+                        {{ __('Update News/Event') }}
+                    </h1>
+
+                    <a href="{{ route('news-and-events.index') }}"
+                        class="bg-green-500 border-green-600 hover:bg-green-700 text-white h-full px-4 py-2 rounded-md">
+                        {{ __('Back') }}
+                    </a>
+                </div>
+
+                <div class="grid grid-cols-2 gap-4">
+                    <form action="{{ route('news-and-events.update', $newsAndEvent->id) }}" method="post" name="NewsAndEvents"
+                        class="bg-white shadow" enctype="multipart/form-data">
+                        @csrf
+                        @method('patch')
+                        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                            <div class="pb-3">
+                                <x-input-label for="title" :value="__('Title')" />
+                                <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title', $newsAndEvent->title)"
+                                    autofocus />
+                            </div>
+
+                            <div class="pb-3">
+                                <x-input-label for="image" :value="__('Image')" />
+                                <x-file-input name="image" id="image"></x-file-input>
+                            </div>
+
+                            <div class="pb-3">
+                                <x-input-label for="name" :value="__('Url')" />
+                                <x-text-input id="name" name="url" type="text" class="mt-1 block w-full" :value="old('url', $newsAndEvent->url)" />
+                            </div>
+
+                            <div class="pb-3">
+                                <x-input-label for="name" :value="__('Text')" />
+                                <x-text-area id="name" name="text" type="text" class="mt-1 block w-full" :value="old('text', $newsAndEvent->text)"
+                                    autofocus />
+                            </div>
+
+                            <div class="buttons-container mt-4">
+                                <x-primary-button>
+                                    {{ __('Add News/Event') }}
+                                </x-primary-button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(function() {
+            $("form[name='NewsAndEvents']").validate({
+                rules: {
+                    title: "required",
+                },
+                messages: {
+                    title: "Title field is required.",
+                },
+                highlight: function(element) {
+                    $(element).removeClass('border-gray-300 focus:border-green-500 focus:ring-green-500')
+                    $(element).addClass('border-red-600 focus:border-red-500 focus:ring-red-500')
+                },
+                unhighlight: function(element) {
+                    $(element).removeClass(
+                        'border-gray-300 border-red-600 focus:border-red-500 focus:ring-red-500')
+                    $(element).addClass('border-green-600 focus:border-green-500 focus:ring-green-500')
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        });
+    </script>
+</x-app-layout>
