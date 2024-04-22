cced24a9ee2d3dcd9585d03a93a625b460ea0d58
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2024-04-22 12:02:28 +0530

    Fixed country auto complete issue

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,17 +3,20 @@ $(function () {
     $('[data-modal-trigger]').click(function () {
         let triggerModal = $(this).attr('data-modal-trigger');
         $(`#overlay`).removeClass('hidden');
+        $('body').addClass('overflow-y-hidden');
         $(`[data-modal=${triggerModal}]`).removeClass('hidden');
     })
 
     /* Close Modal */
     $('.close-modal').click(function () {
         $(`#overlay`).addClass('hidden');
+        $('body').removeClass('overflow-y-hidden');
         $(this).parents('[data-modal]').addClass('hidden');
     })
 
     $("#overlay").click(function () {
         $(`#overlay`).addClass('hidden');
+        $('body').removeClass('overflow-y-hidden');
         $('[data-modal]').addClass('hidden');
     });
 })
